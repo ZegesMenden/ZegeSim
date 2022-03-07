96d82a33cab0c556e89a4157541d679c5b323947
ZegesMenden <51548005+ZegesMenden@users.noreply.github.com>
2022-03-06 22:22:57 -0500

    SHITL!!

--- SHITL.py
+++ SHITL.py
@@ -1,5 +1,5 @@
 # simulation libraries
-
+import msvcrt
 
 from matplotlib.style import available
 from simulation.physics import *
@@ -20,21 +20,75 @@ from datetime import datetime
 
 import serial
 
+com_start = False
+
 with serial.Serial() as ser:
     ser.baudrate = 115200
-    ser.port = 'COM4'
-    ser.open()
+    ser.port = 'COM6'
     ser.timeout=0.5
-ser.open()
-while True:
-    # print(ser.readline())
-    if "a" in str(ser.readline()):
-        print("header recieved")
 
-        buf = []
-        buf = ser.read(32)
-        print(str(len(buf)))
-        print(buf)
+def init_comms():
+
+    print("SITL: initializing communications with callisto")
+
+    ser.open()
+
+    print("SITL: communications established")
+
+print("SITL: initializing")
+print("as if there's anything to initialize lmfao") 
+print("entering main loop, type help for a list of the commands")
+while True:
+    if com_start:
+        inString = ser.readline()
+        if inString:
+            if chr(inString[0]) != '#':
+                print(str(inString)[2:-5])
+            # elif chr(inString[0]) == '#':
+                
+
+    if msvcrt.kbhit():
+        inp = input()
+        if inp.upper() == 'HELP':
+            print('''SITL: HELP:
+exit - exits the program
+help - prints this message
+run - runs the simulation
+init - initializes communications with callisto
+close - stops communications with callisto and closes the serial port
+print <data> - prints data to callisto''')
+        elif inp.upper() == 'EXIT':
+            ser.close()
+            print("SITL: exiting")
+            break
+        elif inp.upper() == 'RUN':
+            if not com_start:
+                print(f"SITL: initializing communication with callisto on {ser.port}")
+                com_start = True
+                init_comms()
+        elif inp.upper() == 'INIT':
+            if not com_start:
+                print("SITL: initializing communications with callisto")
+                com_start = True
+                init_comms()
+            else:
+                print("SITL: communications already established")
+        elif inp.upper() == 'CLOSE':
+            if com_start:
+                print("SITL: closing communications with callisto")
+                com_start = False
+                ser.close()
+        elif 'PRINT' in inp.upper():
+            if com_start:
+                print(f"SITL: printing {inp[6:]}")
+                ser.write(inp[6:].encode())
+            else:
+                print("SITL: communications not established")
+        else:
+            print(f"SITL: WHAT THE FUCK IS {inp} SUPPSED TO MEAN!?")
+
+0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0
+
 # def loop(rocket: rocketBody) -> control_data:
     
 #     cd: control_data = control_data()
--- libs/kalman.py
+++ libs/kalman.py
@@ -1,60 +1,174 @@
+from cv2 import KalmanFilter
 import matplotlib.pyplot as plt
 import numpy as np
 
 # use mmoar than 1 h matrix depending on what data is ready
 
-class kalman:
+class altitudeKF:
 
-    def __init__(self) -> None:
+    def __init__(self):
 
-        self.Xk = np.array([[0.0],  # x
-                            [0.0],  # x.
-                            [0.0]])  # x..
+        self.x = np.array([ [0.0],
+                            [0.0],
+                            [0.0]])
 
-        self.Fk = np.array([[1.0, 0.0, (0.0 ** 2)/2],
-                            [0.0, 1.0, 0.0],
-                            [0.0, 0.0, 1.0]])
+        self.Pk = np.array([[0, 0, 0],
+                            [0, 0, 0],
+                            [0, 0, 0]])
 
-        self.Pk = np.array([[0.0, 0.0, 0.0],
-                            [0.0, 0.0, 0.0],
-                            [0.0, 0.0, 0.0]])
+        self.Qk = np.array([ [1.0, 0, 0],
+                             [0, 0.1, 0],
+                             [0, 0, 0.1]])
 
-        self.Qk = np.array([[0.25, 0.0, 0.0],
-                            [0.0,  0.0, 0.0],
-                            [0.0,  0.0, 5.0]])
+        self.HkAll = np.array([ [1, 0, 0],
+                                [0, 0, 0],
+                                [0, 0, 1]])
 
-        self.Hk = np.array([[1.0, 0.0, 0.0],
-                            [0.0, 0.0, 0.0],
-                            [0.0, 0.0, 0.0]])
+        self.HkBaro = np.array([    [1, 0, 0],
+                                    [0, 0, 0],
+                                    [0, 0, 0]])
 
-        self.Rk = np.array([[15.0, 0.0, 0.0],
-                            [0.0,  5.0, 0.0],
-                            [0.0,  0.0, 0.0]])
+        self.HkAccel = np.array([   [0, 0, 0],
+                                    [0, 0, 0],
+                                    [0, 0, 1]])
 
-        self.K = np.array([[0.0, 0.0, 0.0],
-                            [0.0, 0.0, 0.0],
-                            [0.0, 0.0, 0.0]])
+        self.Rk = np.array([[10.0, 0, 0],
+                            [0, 1, 0],
+                            [0, 0, 0.1]])
 
-        self.Zk = np.array([[0.1], [0.5], [0.7]])
+    
+    def Fk(self, dt):
 
-    def get_kalman_gain(self, Hk):
-        self.K = self.Pk * np.transpose(Hk) * np.linalg.inv(Hk * self.Pk * np.transpose(Hk) + self.Rk)
+        F = np.array([[1, dt, (dt*dt)/2],
+                    [0, 1, dt],
+                    [0, 0, 1]])
 
-    def update_measurement(self):
-        self.Xk = self.Fk * self.Xk + self.K * (self.Zk - self.Hk * self.Xk)
-        self.Pk = self.Pk - (self.K * self.Hk *self.Pk)
+        return F
 
-    def propogate(self, sens, dt):
-        B = np.array([[(dt ** 2)/2],
-                                [dt],
-                                [1]])
-        self.Xk = self.Fk * self.Xk + B * sens
-        self.Pk = self.Fk * self.Pk * np.transpose(self.Fk) + self.Qk
+    def Zk(self, pos, acc):
 
-k: kalman = kalman()
-k.propogate(1, 0.1)
-k.propogate(1.1, 0.1)
-k.propogate(1.01, 0.1)
-k.propogate(1.04, 0.1)
-k.update_measurement()
-k.get_kalman_gain(k.Hk)
+        Z = np.array([  [pos],
+                        [0.0],
+                        [acc]])
+
+        return Z
+
+    def K(self, Hk):
+
+        self.Kk = self.Pk*np.transpose(Hk) * np.linalg.inv((Hk*self.Pk*np.transpose(Hk) + self.Rk))
+
+    def update(self, Hk, pos, acc):
+
+        self.x = self.x + self.Kk*(self.Zk(pos, acc) - Hk*self.x)
+        self.Pk = self.Pk - self.Kk*Hk*self.Pk
+
+    def predict(self, dt):
+
+        self.x = self.Fk(dt)*self.x
+        self.Pk = self.Fk(dt)*self.Pk*np.transpose(self.Fk(dt)) + self.Qk
+
+# def K(Pk, Hk, Rk):
+
+#     K = Pk*np.transpose(Hk) * np.linalg.inv((Hk*Pk*np.transpose(Hk) + Rk))
+
+#     return K
+
+# def update(x, Hk, Pk, K, Zk):
+
+#     x = x + K*(Zk - Hk*x)
+#     Pk = Pk - K*Hk*Pk
+
+#     return x, Pk
+
+# def predict(x, Fk, Pk, Qk):
+
+#     x = Fk*x
+#     Pk = Fk*Pk*np.transpose(Fk) + Qk
+
+#     return x, Pk
+
+position_real = [0]
+position_estimate = [0]
+
+velocity_real = [0]
+velocity_estimate = [0]
+
+acceleration_real = [0]
+acceleration_estimate = [0]
+
+PPlot = [0]
+KPlot = [0]
+
+time = 0.0
+dt= 0.01
+i = 0
+
+baropos = 0.0
+
+KF = altitudeKF()
+
+baropos = np.random.normal(0.0, 1, 1)[0]
+KF.K(KF.HkAll)
+KF.predict(dt)
+KF.update(KF.HkAll, baropos, np.random.normal(0.0, 0.05, 1)[0])
+while True:
+    i += 1
+    time += dt
+
+    accel = 0.0
+
+    vel = velocity_real[-1] + accel*dt
+    pos = position_real[-1] + vel*dt
+
+    position_real.append(pos)
+    velocity_real.append(vel)
+    acceleration_real.append(accel)
+
+    # if i % 10 == 0:
+    baropos = np.random.normal(pos, 0.2, 1)[0]
+    KF.K(KF.HkAll)
+    KF.predict(dt)
+    KF.update(KF.HkAll, baropos, np.random.normal(accel, 0.1, 1)[0])
+
+    # else:
+        # KF.K(KF.HkAll)
+        # KF.predict(dt)
+        # KF.update(KF.HkAccel, 0.0, np.random.normal(accel, 0.05, 1)[0])
+
+        # KF.K(KF.HkAccel)
+        # KF.predict(dt)
+        # KF.update(KF.HkAccel, 0.0, np.random.normal(accel, 0.05, 1)[0])
+        
+
+    print(KF.Pk)
+    print("")
+
+    position_estimate.append(KF.x[0][0])
+    velocity_estimate.append(KF.x[1][1])
+    acceleration_estimate.append(KF.x[2][2])
+
+    KPlot.append(KF.Kk[2][2])
+    PPlot.append(KF.Pk[0][0])
+
+    if time > 10.0:
+        break
+
+t = np.arange(0.0, time+0.01, dt)
+
+plt.figure(1)
+plt.plot(t, position_real, label='real')
+plt.plot(t, position_estimate, label='estimate')
+
+plt.figure(2)
+plt.plot(t, velocity_real, label='real')
+plt.plot(t, velocity_estimate, label='estimate')
+
+plt.figure(3)
+plt.plot(t, acceleration_real, label='real')
+plt.plot(t, acceleration_estimate, label='estimate')
+
+plt.figure(4)
+plt.plot(t, PPlot, label='K')
+
+
+plt.show()
